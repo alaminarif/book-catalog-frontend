f7d76fdff957c7bfd4c96eb6cd95a188b7d280d7
al-amin <arifurr231@gmail.com>
2023-07-18 16:31:23 +0600

    md file added

---
+++ readme.md
@@ -0,0 +1,3 @@
+## [live link:](https://books-catalog.netlify.app/)
+
+# https://books-catalog.netlify.app/
--- src/pages/TopBooks.tsx
+++ src/pages/TopBooks.tsx
@@ -65,7 +65,7 @@ const TopBooks = () => {
           <div className="xl:container m-auto space-y-12 px-6 md:px-12 lg:px-20">
             <div>
               <h2 className="mt-4 text-center text-2xl font-bold   md:text-4xl">
-                Top 10 recently added books <br className="sm:block" hidden />
+                Recently Added Books <br className="sm:block" hidden />
               </h2>
             </div>
             <div className="mt-16 grid gap-8 sm:w-2/3 sm:mx-auto md:w-full md:grid-cols-2 md:-mx-8 lg:grid-cols-3">
